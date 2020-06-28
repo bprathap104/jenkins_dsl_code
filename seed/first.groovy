3437721a2245ab9c679736d27b171d1d1fe8babc
Baskaran <saravanan.baskaran@capgemini.com>
2020-06-28 19:59:46 +0530

    Update first.groovy

--- seed/first.groovy
+++ seed/first.groovy
@@ -1,13 +1,13 @@
 job('first_seed'){
-  steps {
-    scm {
-      git {
-        remote {
-          url('https://github.com/bprathap104/jenkins_dsl_code.git')
-        }
-        branch('master')
+  scm {
+    git {
+      remote {
+        url('https://github.com/bprathap104/jenkins_dsl_code.git')
       }
+      branch('master')
     }
+  }
+  steps {
     dsl {
       text(readFileFromWorkspace('seed/second.groovy'))
       removeAction('DELETE')
